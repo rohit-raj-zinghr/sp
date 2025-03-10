69951829aff59fb9b14537beddbaea8f819344d4
VijayBoura-zinghr <vijay.boura@zinghr.com>
2025-03-10 17:07:21 +0530

    Update final1.sql

--- final1.sql
+++ final1.sql
@@ -269,3 +269,175 @@ SELECT
     @IsValidLocation AS IsValidLocation, 
     @IsValidIP AS IsValidIP,
     @Message AS Message;
+
+
+
+
+
+
+
+
+
+
+create or ALTER PROCEDURE dbo.ValidateEmployeePunch
+
+    @EmpCode VARCHAR(100),
+    @PunchDateTime DATETIME,           -- Single datetime for punch event
+    @PunchIn BIT,                      -- 1 = Validate Punch-In, 0 = Ignore
+    @PunchOut BIT,                     -- 1 = Validate Punch-Out, 0 = Ignore
+    @IPAddress VARCHAR(50),             -- New parameter for IP validation
+    @IsValidPunchIn BIT OUTPUT,
+    @IsValidPunchOut BIT OUTPUT,
+    @Message VARCHAR(500) OUTPUT
+
+AS
+BEGIN
+    SET NOCOUNT ON;
+ 
+    -- Declare variables
+    DECLARE @ShiftID VARCHAR(50);
+    DECLARE @InTime TIME;
+    DECLARE @OutTime TIME;
+    DECLARE @PreTime INT;
+    DECLARE @PostTime INT;
+    DECLARE @ShiftStartDate DATE;
+    DECLARE @ShiftEndDate DATE;
+    DECLARE @ShiftDetailsJson NVARCHAR(MAX);
+    DECLARE @IsIPValid BIT;
+ 
+    -- Punch validation variables
+
+    DECLARE @PunchTime TIME = CAST(@PunchDateTime AS TIME);
+    DECLARE @PunchDate DATE = CAST(@PunchDateTime AS DATE);
+ 
+    -- Initialize outputs
+
+    SET @IsValidPunchIn = 0;
+    SET @IsValidPunchOut = 0;
+    SET @Message = '';
+ 
+    -- Validate input: Only one of PunchIn or PunchOut should be 1
+
+    IF (@PunchIn = 1 AND @PunchOut = 1) OR (@PunchIn = 0 AND @PunchOut = 0)
+    BEGIN
+        SET @Message = 'Invalid input: Specify either PunchIn = 1 or PunchOut = 1, but not both or neither.';
+        RETURN;
+    END;
+ 
+    -- **Step 1: Validate Employee IP**
+
+    EXEC dbo.CheckEmployeeIPValidation 
+        @EmpCode = @EmpCode, 
+        @IPAddress = @IPAddress, 
+        @IsValid = @IsIPValid OUTPUT;
+    IF @IsIPValid = 0
+    BEGIN
+        SET @Message = 'Punch rejected: Invalid IP address ' + @IPAddress + ' for employee ' + @EmpCode;
+        RETURN;
+
+    END;
+ 
+    -- **Step 2: Get ShiftDetails JSON**
+    SELECT @ShiftDetailsJson = ShiftDetails FROM dbo.FlatTable WHERE EmpCode = @EmpCode;
+ 
+    IF @ShiftDetailsJson IS NULL
+    BEGIN
+        SET @Message = 'No shift details found for employee ' + @EmpCode;
+        RETURN;
+    END;
+ 
+    IF ISJSON(@ShiftDetailsJson) = 0
+    BEGIN
+        SET @Message = 'Invalid JSON format in ShiftDetails for employee ' + @EmpCode + ': ' + LEFT(@ShiftDetailsJson, 100);
+        RETURN;
+    END;
+ 
+    -- **Step 3: Parse JSON to Extract Shift Details**
+    BEGIN TRY
+
+        -- Select the first matching shift based on date range
+        SELECT TOP 1
+            @ShiftID = JSON_VALUE(shift.value, '$.ShiftID'),
+            @InTime = CAST(JSON_VALUE(JSON_QUERY(shift.value, '$.ShiftDetails'), '$.InTime') AS TIME),
+            @OutTime = CAST(JSON_VALUE(JSON_QUERY(shift.value, '$.ShiftDetails'), '$.OutTime') AS TIME),
+            @PreTime = COALESCE(CAST(JSON_VALUE(JSON_QUERY(shift.value, '$.ShiftDetails'), '$.PreTime') AS INT), 0),
+            @PostTime = COALESCE(CAST(JSON_VALUE(JSON_QUERY(shift.value, '$.ShiftDetails'), '$.PostTime') AS INT), 0),
+            @ShiftStartDate = CAST(JSON_VALUE(range.value, '$.RangeStart') AS DATE),
+            @ShiftEndDate = CAST(JSON_VALUE(range.value, '$.RangeEnd') AS DATE)
+        FROM OPENJSON(@ShiftDetailsJson) AS shift
+        CROSS APPLY OPENJSON(JSON_QUERY(shift.value, '$.ShiftDetails'), '$.ShiftRanges') AS range
+        WHERE @PunchDate BETWEEN CAST(JSON_VALUE(range.value, '$.RangeStart') AS DATE)
+                            AND CAST(JSON_VALUE(range.value, '$.RangeEnd') AS DATE)
+        IF @ShiftID IS NULL
+        BEGIN
+            SET @Message = 'No applicable shift found for employee ' + @EmpCode + ' on date ' + CONVERT(VARCHAR, @PunchDate, 23) + '. JSON snippet: ' + LEFT(@ShiftDetailsJson, 100);
+            RETURN;
+        END;
+
+    END TRY
+    BEGIN CATCH
+        SET @Message = 'Error parsing JSON for employee ' + @EmpCode + ': ' + ERROR_MESSAGE() + ' at line ' + CAST(ERROR_LINE() AS VARCHAR(10)) + '. JSON snippet: ' + LEFT(@ShiftDetailsJson, 100);
+        RETURN;
+    END CATCH;
+ 
+    -- **Step 4: Compute Valid Punch Windows**
+    DECLARE @ValidPunchInStart TIME = DATEADD(MINUTE, -@PreTime, @InTime);
+    DECLARE @ValidPunchInEnd TIME = @InTime;
+    DECLARE @ValidPunchOutStart TIME = @OutTime;
+    DECLARE @ValidPunchOutEnd TIME = DATEADD(MINUTE, @PostTime, @OutTime);
+ 
+    -- **Step 5: Validate Punch Timing**
+
+    IF @PunchIn = 1
+
+    BEGIN
+        IF @PunchTime BETWEEN @ValidPunchInStart AND @ValidPunchInEnd
+        BEGIN
+            SET @IsValidPunchIn = 1;
+            SET @Message = 'Punch-In at ' + CONVERT(VARCHAR, @PunchTime, 108) + 
+
+                           ' is valid for ShiftID ' + @ShiftID;
+
+        END
+        ELSE
+        BEGIN
+            SET @Message = 'Invalid Punch-In time. Allowed: ' + 
+
+                           CONVERT(VARCHAR, @ValidPunchInStart, 108) + 
+
+                           ' to ' + CONVERT(VARCHAR, @ValidPunchInEnd, 108) + '.';
+
+        END;
+    END
+    ELSE IF @PunchOut = 1
+    BEGIN
+        IF @PunchTime BETWEEN @ValidPunchOutStart AND @ValidPunchOutEnd
+        BEGIN
+            SET @IsValidPunchOut = 1;
+            SET @Message = 'Punch-Out at ' + CONVERT(VARCHAR, @PunchTime, 108) + 
+
+                           ' is valid for ShiftID ' + @ShiftID;
+        END
+        ELSE
+        BEGIN
+            SET @Message = 'Invalid Punch-Out time. Allowed: ' + 
+                           CONVERT(VARCHAR, @ValidPunchOutStart, 108) + 
+                           ' to ' + CONVERT(VARCHAR, @ValidPunchOutEnd, 108) + '.';
+        END;
+    END;
+END;
+GO
+ 
+DECLARE @IsValidPunchIn BIT, @IsValidPunchOut BIT, @Message VARCHAR(500)
+EXEC dbo.ValidateEmployeePunch
+    @EmpCode = '00000003',
+    @PunchDateTime = '2023-05-02 17:30:00',
+    @PunchIn = 0,
+    @PunchOut = 1,
+    @IPAddress = '1.1.1.9', -- IP to validate
+    @IsValidPunchIn = @IsValidPunchIn OUTPUT,
+    @IsValidPunchOut = @IsValidPunchOut OUTPUT,
+    @Message = @Message OUTPUT;
+SELECT @IsValidPunchIn AS IsValidPunchIn, @IsValidPunchOut AS IsValidPunchOut, @Message AS Message;
+ 
+ 
